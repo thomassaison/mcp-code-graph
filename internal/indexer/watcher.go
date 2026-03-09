9338045105895fb80d54bbf70166a1f9f81fb487
Thomas Saison <tsaison9@gmail.com>
2026-03-09 18:13:57 +0100

    feat(indexer): add file watcher with debouncing

---
+++ internal/indexer/watcher.go
@@ -0,0 +1,95 @@
+package indexer
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+type Watcher struct {
+	indexer  *Indexer
+	watcher  *fsnotify.Watcher
+	debounce time.Duration
+	done     chan struct{}
+}
+
+func NewWatcher(idx *Indexer, debounce time.Duration) (*Watcher, error) {
+	fsWatcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
+	}
+
+	w := &Watcher{
+		indexer:  idx,
+		watcher:  fsWatcher,
+		debounce: debounce,
+		done:     make(chan struct{}),
+	}
+
+	go w.processEvents()
+
+	return w, nil
+}
+
+func (w *Watcher) Watch(dir string) error {
+	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			if strings.HasPrefix(info.Name(), ".") || info.Name() == "vendor" {
+				return filepath.SkipDir
+			}
+			return w.watcher.Add(path)
+		}
+		return nil
+	})
+}
+
+func (w *Watcher) processEvents() {
+	var pending map[string]bool
+	var timer *time.Timer
+
+	for {
+		select {
+		case <-w.done:
+			return
+		case event, ok := <-w.watcher.Events:
+			if !ok {
+				return
+			}
+			if !strings.HasSuffix(event.Name, ".go") {
+				continue
+			}
+			if pending == nil {
+				pending = make(map[string]bool)
+			}
+			pending[event.Name] = true
+
+			if timer != nil {
+				timer.Stop()
+			}
+			snapshot := pending
+			pending = nil
+			timer = time.AfterFunc(w.debounce, func() {
+				for path := range snapshot {
+					w.indexer.IndexFile(path)
+				}
+			})
+
+		case err, ok := <-w.watcher.Errors:
+			if !ok {
+				return
+			}
+			_ = err
+		}
+	}
+}
+
+func (w *Watcher) Close() {
+	close(w.done)
+	w.watcher.Close()
+}
